3503926da610e8e8c0cf5bbc750165b84195ba57
Bruno Queiroz <bruno.queiroz@dclick.com.br>
2015-03-18 22:16:09 -0300

    ajustes na geração dos relatórios de cobertura

--- karma.conf.js
+++ karma.conf.js
@@ -36,9 +36,19 @@ module.exports = function(config) {
     },
 
     coverageReporter: {
-      type : 'lcov',
-      dir : 'coverage/',
-      file: 'report-lcov.lcov'
+      dir : 'coverage',
+      reporters: [
+        // reporters not supporting the `file` property
+        { type: 'html', subdir: 'report-html' },
+        //{ type: 'lcov', subdir: 'report-lcov' },
+        // reporters supporting the `file` property, use `subdir` to directly
+        // output them in the `dir` directory
+        //{ type: 'cobertura', subdir: '.', file: 'cobertura.txt' },
+        { type: 'lcovonly', subdir: '.', file: 'report-lcov.lcov' },
+        //{ type: 'teamcity', subdir: '.', file: 'teamcity.txt' },
+        //{ type: 'text', subdir: '.', file: 'text.txt' },
+        //{ type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
+      ]
     }
 
   };
