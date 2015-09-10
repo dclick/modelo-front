4878e68cd3a93ee81aa191f9a9e0622de39e2087
Weslley Neri <weslley39@gmail.com>
2015-09-10 14:49:39 -0300

    removida variavel do gestor

--- sesc-config/modelo/config.js
+++ sesc-config/modelo/config.js
@@ -1,6 +1,5 @@
 window.config = {
   APP_BASE_URL                   : "http://localhost:3000/api/",
   APP_USER_NOT_AUTH_REDIRECT     : 'login',
-  APP_OTHERWISE_URL              : '/solicitacao',
-  APP_BASE_URL_GESTOR_ACESSO     : 'http://webapps.javadev.sescsp.org.br/permissao'
+  APP_OTHERWISE_URL              : '/solicitacao'
 };
